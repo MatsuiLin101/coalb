a272ab448ff7b7e5d7eb4b5d04cc8b2f2808499d
MatsuiLin101 <matsuilin101@gmail.com>
2019-12-20 17:21:49 +0800

    Reply message decorator

--- apps/lines/views.py
+++ apps/lines/views.py
@@ -39,3 +39,11 @@ def callback(request):
         return HttpResponse(status=400, content=response)
 
     return HttpResponse(status=200, content='OK')
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
