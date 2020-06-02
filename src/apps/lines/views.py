6f63c091bffebd427badcc0e6a0bebf41eda0297
MatsuiLin101 <matsuilin101@gmail.com>
2020-06-02 18:12:20 +0800

    Fix get user profile

--- src/apps/lines/views.py
+++ src/apps/lines/views.py
@@ -94,6 +94,7 @@ def handle_follow(event):
     print(f"Type is {event.type}")
     print(f"Sources is {event.source}")
     userId = event.source.userId
+    profile = line_bot_api.get_profile(userId)
     try:
         line_user = LineUser.objects.get(userId=userId)
         if line_user.status is False:
