c8fb15650c1ac0c76ddc1ff0f382121374c87f50
MatsuiLin101 <matsuilin101@gmail.com>
2020-06-02 19:52:07 +0800

    Add chromedriver path

--- src/apps/coas/utils.py
+++ src/apps/coas/utils.py
@@ -65,11 +65,11 @@ def pre_process_text_3(split_text):
 
 
 def get_book(key):
-    # chrome = 'chromedriver'
+    chrome = '/usr/bin/chromedriver'
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('User-Agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36"')
-    driver = webdriver.Chrome(options=chrome_options) # executable_path=chrome, 
+    driver = webdriver.Chrome(executable_path=chrome, options=chrome_options)
     url = 'https://agrstat.coa.gov.tw/sdweb/public/book/'
     aspx = 'Book.aspx'
 
