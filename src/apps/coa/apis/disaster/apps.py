c03574220e3514278a1497a28313cc9281e99dff
MatsuiLin101 <matsuilin101@gmail.com>
2021-04-12 11:30:58 +0800

    Remove coa api apps

--- src/apps/coa/apis/disaster/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DisasterConfig(AppConfig):
-    name = 'disaster'
--- src/apps/coa/apis/livestockslaughter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SlaughterConfig(AppConfig):
-    name = 'slaughter'
