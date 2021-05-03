04de2f96ef13c407d591f3221b20c2d3f90f00ba
MatsuiLin101 <matsuilin101@gmail.com>
2021-05-04 01:04:44 +0800

    Optimate apiview class

--- src/apps/coa/apis/value.py
+++ src/apps/coa/apis/value.py
@@ -30,27 +30,6 @@ class ValueApiView(ApiView):
         os.remove(self.xlsx_name) if self.xlsx_name else None
         return self.message
 
-    def download(self):
-        # download ods and transfer to xlsx
-        driver = get_driver()
-        try:
-            driver.get(self.url)
-            WebDriverWait(driver, 30, 0.1).until(EC.presence_of_element_located((By.ID, self.id_book)))
-            driver.find_element(By.ID, self.id_book).click()
-            ods = driver.find_element(By.ID, self.id_ods)
-            ods_href = ods.get_attribute("href")
-
-            ods_name = f"value_{int(datetime.datetime.now().timestamp())}.ods"
-            res = requests.get(ods_href)
-            with open(ods_name, 'wb') as f:
-                f.write(res.content)
-            subprocess.Popen(f'{settings.LIBREOFFICE_PATH} --headless --invisible --convert-to xlsx {ods_name}', shell=True, stderr=subprocess.PIPE).communicate()
-            self.xlsx_name = ods_name.replace('ods', 'xlsx')
-            os.remove(ods_name)
-        except Exception as e:
-            print(e)
-        driver.close()
-
     def open_wb(self):
         # open value xslx
         wb = load_workbook(filename=self.xlsx_name)
