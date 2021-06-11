0c2a7d68b2c11ffb04af0cd543da6942dcdbbc60
MatsuiLin101 <matsuilin101@gmail.com>
2021-06-11 12:11:12 +0800

    Modify coa apis import packages

---
+++ src/apps/coa/apis/configs.py
@@ -0,0 +1,4 @@
+import os
+
+from apps.coa.configs import ApiView
+from apps.coa.utils import *
--- src/apps/coa/apis/gross.py
+++ src/apps/coa/apis/gross.py
@@ -1,7 +1,4 @@
-import os
-
-from apps.coa.configs import ApiView
-from apps.coa.utils import *
+from .configs import *
 
 
 class GrossApiView(ApiView):
