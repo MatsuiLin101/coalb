b5f2d504fa4fcdc8a9b987e15f0190a90378cf76
MatsuiLin101 <matsuilin101@gmail.com>
2021-06-28 21:20:36 +0800

    Livestockfeedamount finish

--- src/apps/coa/apis/livestockfeedlot.py
+++ src/apps/coa/apis/livestockfeedlot.py
@@ -5,7 +5,7 @@ class LivestockFeedlotApiView(ApiView):
     '''
     畜禽飼養場數api介面
     -feedlot(飼養場數)
-    動態查詢 [農業生產統計]>>[畜禽產品飼養數量統計]>>[家畜飼養場數]、[家禽飼養場數：縣市別×家禽別(104年起)]
+    動態查詢 [農業生產統計]>>[畜禽產品飼養數量統計]>>[家畜飼養場數]、[家禽飼養場數：縣市別×家禽別(104年度起)]
     https://agrstat.coa.gov.tw/sdweb/public/inquiry/InquireAdvance.aspx
     '''
     def __init__(self, query_date, product, city=None):
