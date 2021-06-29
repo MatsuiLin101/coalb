97329d5906d755692dbb8b775fda07e37e24587a
MatsuiLin101 <matsuilin101@gmail.com>
2021-06-29 11:18:05 +0800

    Build livestock byproduct models

--- src/apps/coa/builders/__init__.py
+++ src/apps/coa/builders/__init__.py
@@ -1,8 +1,11 @@
 from .livestockfeedlot import *
 from .livestockfeedamount import *
+from .livestockslaughter import *
+from .livestockbyproduct import *
 
 
 def build():
     LivestockFeedlotBuilder().build()
     LivestockFeedamountBuilder().build()
     LivestockSlaughterBuilder().build()
+    LivestockByproductBuilder().build()
