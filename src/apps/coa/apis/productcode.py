ab2b5ff1ce6129c3e80546731faafa6c76a24fed
MatsuiLin101 <matsuilin101@gmail.com>
2021-06-30 17:42:57 +0800

    Api product code finish

--- src/apps/coa/apis/productcode.py
+++ src/apps/coa/apis/productcode.py
@@ -7,3 +7,20 @@ class ProductCodeApiView(ApiView):
     (NEW)productcode(作物代碼)
     EXCEL匯入
     '''
+    def __init__(self, product):
+        self.product = product
+        self.message = ""
+
+    def api(self):
+        try:
+            qs = ProductCode.objects.filter(name__icontains=self.product)
+            if qs.count() == 0:
+                self.message = f"搜尋「代碼 {self.product}」查無結果"
+            else:
+                message_list = list()
+                for obj in qs:
+                    message_list.append(f"{obj.category}：{obj.code} {obj.name}")
+                self.message = f"{self.product} 代碼：\n" + "\n".join(message for message in message_list)
+        except Exception as e:
+            self.message = f"搜尋「代碼 {self.product}」發生錯誤"
+        return self.message
