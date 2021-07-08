b3630778c9aaf1f2cd63d789bb25238758d1cebd
MatsuiLin101 <matsuilin101@gmail.com>
2021-07-08 12:02:45 +0800

    Review user command reply

--- src/apps/coa/views.py
+++ src/apps/coa/views.py
@@ -1,8 +1,13 @@
+import traceback
+
 from openpyxl import load_workbook
 
 from django.shortcuts import render
 
-from apps.coa.execute_apis import *
+from apps.log.models import TracebackLog
+
+from apps.coa.apis import *
+from apps.coa.apis.configs import CustomError
 from apps.coa.utils import (
     pre_process_text_2,
     query_produce_value,
@@ -11,172 +16,115 @@ from apps.coa.utils import (
 from apps.coa.models import ProductCode, CropProduceUnit
 
 
-def api_view(text):
+def api_view(command_text):
     # 將輸入的文字依據空格分割
-    text_list = text.split(' ')
-    if len(text_list) == 2:
-        command, year = text_list
-        city_product = None
-        product = None
-    elif len(text_list) == 3:
-        command, year, city_product = text_list
-        city = None
-    elif len(text_list) == 4:
-        command, year, city_product, city = text_list
-    else:
-        command = None
-        year = None
-        city = None
-        city_product = None
+    list_text = command_text.strip().split(' ')
+    list_params = list()
+    for text in list_text:
+        if len(text) > 0:
+            list_params.append(text)
 
+    command = list_params[0]
     # 判斷分割後的第一組文字是哪一個指令
     if command == "總產值":
         response = query_produce_value(year, city_product)
         reply = f"{text}\n{response}"
-    elif command == "產值":
-        api = ValueApiView(year, city_product)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command == "生產毛額":
-        api = GrossApiView(year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command == "農家所得":
-        api = IncomeApiView(year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command  == "農牧戶":
-        api = FarmerApiView(year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command == "耕地面積":
-        api = FarmerAreaApiView(year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command == "災害":
-        api = DisasterApiView(year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command == "就業人口":
-        api = LaborforceApiView(year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command in ["農保", "老農津貼", "獎助學金"]:
-        api = WelfareApiView(command, year, city_product)
-        response = api.choose_api().execute_api()
-        reply = f"{text}\n{response}"
+    elif command in ["產值"]:
+        apiview = ValueApiView
+    elif command in ["毛額", "生產毛額"]:
+        apiview = GrossApiView
+    elif command in ["所得", "農家所得"]:
+        apiview = IncomeApiView
+    elif command  in ["農牧戶人口數", "農牧戶戶數", "人口數", "戶數", "農牧戶"]:
+        apiview = FarmerApiView
+    elif command in ["耕地面積"]:
+        apiview = FarmerAreaApiView
+    elif command in ["勞動力", "就業人口"]:
+        apiview = LaborforceApiView
+    elif command in ["災害"]:
+        apiview = DisasterApiView
+    elif command in ["農保", "津貼", "老農津貼", "獎助學金"]:
+        apiview = WelfareApiView
     elif command in ["產地", "批發"]:
-        api = CropPriceApiView(command, year, city_product, city)
-        response = api.choose_api_api().execute_api()
-        reply = f"{text}\n{response}"
+        apiview = CropPriceApiView
     elif command in ["產量", "種植面積", "單位產值", "單位產量"]:
-        api = CropProduceApiView(command, year, city, city_product)
-        response = api.choose_api().execute_api()
-        reply = f"{text}\n{response}"
-    elif command in ["生產成本", "生產費用", "粗收益", "淨收入率", "工時"]:
-        api = CropCostApiView(command, year, city_product)
-        response = api.choose_api().execute_api()
-        reply = f"{text}\n{response}"
-    elif command in ["交易量", "價格", "重量"]:
-        api = LivestockHogApiView(command, year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
+        apiview = CropProduceApiView
+    elif command in ["成本", "生產成本", "費用", "生產費用", "粗收益", "淨收入率", "工時"]:
+        apiview = CropCostApiView
+    elif command in ["毛豬", "交易量", "價格", "重量"]:
+        apiview = LivestockHogApiView
     elif command in ["拍賣價", "產地價", "零售價"]:
-        api = LivestockPriceApiView(command, year, city_product)
-        response = api.choose_api().execute_api()
-        reply = f"{text}\n{response}"
-    elif command in ["場數"]:
-        api = LivestockFeedlotApiView(year, city_product, city)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command in ["在養量"]:
-        api = LivestockFeedamountApiView(year, city_product, city)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command in ["屠宰"]:
-        api = LivestockSlaughterApiView(year, city_product, city)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
+        apiview = LivestockPriceApiView
+    elif command in ["場數", "飼養場數"]:
+        apiview = LivestockFeedlotApiView
+    elif command in ["在養", "在養量"]:
+        apiview = LivestockFeedamountApiView
+    elif command in ["屠宰", "屠宰量"]:
+        apiview = LivestockSlaughterApiView
     elif command in ["副產", "副產品", "副產物"]:
-        api = LivestockByproductApiView(year, city_product, city)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    elif command in ["代碼"]:
-        api = ProductCodeApiView(year)
-        response = api.execute_api()
-        reply = f"{text}\n{response}"
-    # elif '產地' in text or '批發' in text or '零售' in text:
-    #     reply = parser_product(text)
-        # if reply is False:
-        #     reply = '請輸入年+空格+關鍵字\n例如：\n107 產值 總產值\n107 產值 豬\n107 農家所得\n107 農牧戶\n107 耕地面積'
-    elif "指令" in text:
-        reply = f"目前提供的指令如下(括號內請改成要查詢的內容，並將底線以空白一格替換)：\n"
-        reply += "總產值_(年份)\n"
-        reply += "總產值_(年份)_(城市)\n"
-        reply += "產值_(年份)_(農產品/畜產品)\n"
-        # reply += "產值_(年份)_(城市)_(農產品/畜產品)\n"
-        reply += "生產毛額_(年份)\n"
-        reply += "農家所得_(年份)\n"
-        reply += "農牧戶_(年份)\n"
-        reply += "耕地面積_(年份)\n"
-        reply += "災害_(年份)\n"
-        reply += "就業人口_(年份)\n就業人口_(年份)/(月份)\n"
-        reply += "老農津貼_(年份)/(月份)\n農保_(年份)/(月份)_(城市)\n"
-        reply += "獎助學金_(年份)\n"
-        reply += "產量_(年份)_(縣市鄉鎮)_(作物)\n"
-        reply += "種植面積_(年份)_(縣市鄉鎮)_(作物)\n"
-        reply += "生產成本_(年份)_(作物)\n"
-        reply += "生產費用_(年份)_(作物)\n"
-        reply += "粗收益_(年份)_(作物)\n"
-        reply += "淨收入率_(年份)_(作物)\n"
-        reply += "工時_(年份)_(作物)\n"
-        reply += "交易量_(年份)_豬\n"
-        reply += "價格_(年份)_豬\n"
-        reply += "重量_(年份)_豬\n"
-        reply += "拍賣價_(年份)_(畜禽)\n"
-        reply += "產地價_(年份)_(畜禽)\n"
-        reply += "零售價_(年份)_(畜禽)\n"
-        reply += "場數_(年份)_(畜禽)_(城市)\n"
-        reply += "在養量_(年份)_(畜禽)_(城市)\n"
-        reply += "屠宰_(年份)_(畜禽)_(城市)\n"
-        reply += "產量_(年份)_(畜禽)_(城市)\n"
-        reply += "代碼_(作物)\n"
+        apiview = LivestockByproductApiView
+    elif command in ["代碼", "作物代碼"]:
+        apiview = ProductCodeApiView
+    elif "指令" in command_text:
+        reply = f"直接輸入指令可以查詢使用方式(括號內為備註不需輸入)，目前提供的指令如下\n\n"
+        reply += "經濟指標及其他類：\n"
+        reply += "產值\n"
+        reply += "總產值\n"
+        reply += "生產毛額\n"
+        reply += "農家所得\n"
+        reply += "農牧戶(包含戶數及人口數)\n"
+        reply += "耕地面積\n"
+        reply += "就業人口\n"
+        reply += "災害\n"
+        reply += "農保\n"
+        reply += "老農津貼\n"
+        reply += "獎助學金\n\n"
+
+        reply += "農耕類：\n"
+        reply += "產地\n"
+        reply += "批發\n"
+        reply += "產量\n"
+        reply += "種植面積\n"
+        reply += "單位產值\n"
+        reply += "單位產量\n"
+        reply += "生產成本(包含生產費用、粗收益、淨收入率、工時)\n\n"
+
+        reply += "畜禽類：\n"
+        reply += "毛豬(包含交易量、拍賣價、平均重量)\n"
+        reply += "拍賣價\n"
+        reply += "產地價\n"
+        reply += "零售價\n"
+        reply += "飼養場數\n"
+        reply += "在養量\n"
+        reply += "屠宰量\n"
+        reply += "產量\n\n"
+
+        reply += "其他類：\n"
+        reply += "作物代碼"
+        return reply
     else:
-        reply = f"很抱歉，您輸入的指令「{text}」可能有誤，無法為您查詢，目前可使用的查詢指令如下(括號內請改成要查詢的內容，並將底線以空白一格替換)：\n"
-        reply += "總產值_(年份)\n"
-        reply += "總產值_(年份)_(城市)\n"
-        reply += "產值_(年份)_(農產品/畜產品)\n"
-        # reply += "產值_(年份)_(城市)_(農產品/畜產品)\n"
-        reply += "生產毛額_(年份)\n"
-        reply += "農家所得_(年份)\n"
-        reply += "農牧戶_(年份)\n"
-        reply += "耕地面積_(年份)\n"
-        reply += "災害_(年份)\n"
-        reply += "就業人口_(年份)\n就業人口_(年份)/(月份)\n"
-        reply += "老農津貼_(年份)/(月份)\n農保_(年份)/(月份)_(城市)\n"
-        reply += "獎助學金_(年份)\n"
-        reply += "產量_(年份)_(縣市鄉鎮)_(作物)\n"
-        reply += "種植面積_(年份)_(縣市鄉鎮)_(作物)\n"
-        reply += "生產成本_(年份)_(作物)\n"
-        reply += "生產費用_(年份)_(作物)\n"
-        reply += "粗收益_(年份)_(作物)\n"
-        reply += "淨收入率_(年份)_(作物)\n"
-        reply += "工時_(年份)_(作物)\n"
-        reply += "交易量_(年份)_豬\n"
-        reply += "價格_(年份)_豬\n"
-        reply += "重量_(年份)_豬\n"
-        reply += "拍賣價_(年份)_(畜禽)\n"
-        reply += "產地價_(年份)_(畜禽)\n"
-        reply += "零售價_(年份)_(畜禽)\n"
-        reply += "場數_(年份)_(畜禽)_(城市)\n"
-        reply += "在養量_(年份)_(畜禽)_(城市)\n"
-        reply += "屠宰_(年份)_(畜禽)_(城市)\n"
-        reply += "產量_(年份)_(畜禽)_(城市)\n"
-        reply += "代碼_(作物)\n"
+        reply = f"很抱歉，您輸入的指令「{text}」可能有誤，無法為您查詢，輸入「指令」可以查詢目前可用的指令。"
+        return reply
+
+    try:
+        obj = apiview(list_params)
+        if hasattr(obj, 'choose_api'):
+            obj = obj.choose_api()
+        reply = f"搜尋「{command_text}」結果如下\n" + obj.execute_api()
+    except CustomError as ce:
+        reply = f"無法搜尋「{command_text}」\n" + str(ce)
+    except Exception as e:
+        traceback_log = TracebackLog.objects.create(app="api_view", message=traceback.format_exc())
+        reply = f"搜尋「{command_text}」發生未知錯誤，錯誤編號「{traceback_log.id}」，請通知管理員處理"
+        raise CustomError(reply)
+
     return reply
 
 
 def file_view_product_code(file_name):
+    '''
+    處理上傳的代碼excel
+    '''
     try:
         ProductCode.objects.all().delete()
         main_count = 0
@@ -209,6 +157,9 @@ def file_view_product_code(file_name):
 
 
 def file_view_crop_produce(file_name):
+    '''
+    處理上傳的產量產值表
+    '''
     CropProduceUnit.objects.all().delete()
     wb = load_workbook(file_name)
     for sheet in wb.sheetnames:
