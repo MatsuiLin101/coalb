dbe9de1de518ebf05d8636f7c65e79aa9c443a59
MatsuiLin101 <matsuilin101@gmail.com>
2021-07-20 21:05:05 +0800

    temp7

--- src/apps/coa/apis/cropprice.py
+++ src/apps/coa/apis/cropprice.py
@@ -93,6 +93,7 @@ class CropPriceOriginApiView(CropPriceApiView):
             #     'params': self.params
             # }
             data = "&".join(params for params in self.params)
+            return data
             res = requests.get(f"{settings.PROXY_DOMAIN}{reverse('coa:proxy_parser')}?token={settings.PROXY_TOKEN}&data={data}")
             return res.text
         else:
