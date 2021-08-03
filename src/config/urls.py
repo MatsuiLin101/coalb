e239d0e704971560d42b184be8bb211cd5aebfb6
MatsuiLin101 <matsuilin101@gmail.com>
2021-08-03 16:55:37 +0800

    Page not found

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from . import views
 
 
 urlpatterns = [
@@ -30,3 +32,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^', views.page_not_found, name='page_not_found'),
+]
