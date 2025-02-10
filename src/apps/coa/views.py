e70a82f683f1d5e1af87a6f4fe1c33c75219a1e5
MatsuiLin101 <matsuilin101@gmail.com>
2025-02-10 16:05:58 +0800

    feat: rename api view argument

--- src/apps/coa/views.py
+++ src/apps/coa/views.py
@@ -22,7 +22,7 @@ from apps.coa.utils import CustomError, get_driver
 from apps.coa.models import ProductCode, CropProduceUnit, LivestockByproduct, CropProduceTotal
 
 
-def api_view(command_text):
+def get_reply_from_text(command_text):
     # 將輸入的文字依據空格分割
     list_text = command_text.strip().split(' ')
     list_params = list()
@@ -33,41 +33,41 @@ def api_view(command_text):
     command = list_params[0]
     # 判斷分割後的第一組文字是哪一個指令
     if command in ["產值", "總產值"]:
-        apiview = ProduceValueApiView
+        api_view = ProduceValueApiView
     elif command in ["毛額", "生產毛額"]:
-        apiview = GrossApiView
+        api_view = GrossApiView
     elif command in ["所得", "農家所得"]:
-        apiview = IncomeApiView
+        api_view = IncomeApiView
     elif command  in ["農牧戶人口數", "農牧戶戶數", "人口數", "戶數", "農牧戶"]:
-        apiview = FarmerApiView
+        api_view = FarmerApiView
     elif command in ["耕地面積"]:
-        apiview = FarmerAreaApiView
+        api_view = FarmerAreaApiView
     elif command in ["勞動力", "就業人口"]:
-        apiview = LaborforceApiView
+        api_view = LaborforceApiView
     elif command in ["災害"]:
-        apiview = DisasterApiView
+        api_view = DisasterApiView
     elif command in ["農保", "津貼", "老農津貼", "獎助學金"]:
-        apiview = WelfareApiView
+        api_view = WelfareApiView
     elif command in ["產地", "批發"]:
-        apiview = CropPriceApiView
+        api_view = CropPriceApiView
     elif command in ["種植面積", "單位產值", "單位產量"]:
-        apiview = CropProduceApiView
+        api_view = CropProduceApiView
     elif command in ["成本", "生產成本", "費用", "生產費用", "粗收益", "淨收入率", "工時"]:
-        apiview = CropCostApiView
+        api_view = CropCostApiView
     elif command in ["毛豬", "交易量", "價格", "重量"]:
-        apiview = LivestockHogApiView
+        api_view = LivestockHogApiView
     elif command in ["拍賣價", "產地價", "零售價"]:
-        apiview = LivestockPriceApiView
+        api_view = LivestockPriceApiView
     elif command in ["場數", "飼養場數"]:
-        apiview = LivestockFeedlotApiView
+        api_view = LivestockFeedlotApiView
     elif command in ["在養", "在養量"]:
-        apiview = LivestockFeedamountApiView
+        api_view = LivestockFeedamountApiView
     elif command in ["屠宰", "屠宰量"]:
-        apiview = LivestockSlaughterApiView
+        api_view = LivestockSlaughterApiView
     elif command in ["副產", "副產品", "副產物"]:
-        apiview = LivestockByproductApiView
+        api_view = LivestockByproductApiView
     elif command in ["代碼", "作物代碼"]:
-        apiview = ProductCodeApiView
+        api_view = ProductCodeApiView
     elif command in ["產量"]:
         reply = f"無法搜尋「{command_text}」\n\n"
         reply += f"產量（作物產量）的指令為「產量 縣市 品項 年份」可加上鄉鎮「產量 縣市鄉鎮 品項 年份」，例如：\n"
@@ -75,40 +75,41 @@ def api_view(command_text):
         reply += f"產量（副產物產量）的指令為「產量 品項 年份」或「產量 品項 年份 縣市」，也可使用替代指令「副產物」或「副產品」，例如：\n"
         reply += f"「產量 雞蛋 108」\n「產量 牛乳 109 雲林」\n"
         reply += f"「副產物 蜂蜜 108」\n「副產品 蜂蜜 105 彰化」"
-        if not 3 <= len(list_params) <= 4:
+
+        if len(list_params) < 3 or len(list_params) > 4:
             return reply
 
-        apiview = None
+        api_view = None
+
         if len(list_params) == 3:
             command_text = command_text.replace("產量", "產量（副產物產量）")
-            apiview = LivestockByproductApiView
+            api_view = LivestockByproductApiView
 
         if len(list_params) == 4:
             product = list_params[1]
             query_set = LivestockByproduct.objects.filter(name__icontains=product, sub_class="product")
             if query_set.count() > 0:
                 command_text = command_text.replace("產量", "產量（副產物產量）")
-                apiview = LivestockByproductApiView
+                api_view = LivestockByproductApiView
 
             query_set = CropProduceTotal.objects.filter(name__icontains=product)
             if query_set.count() > 0:
                 command_text = command_text.replace("產量", "產量（作物產量）")
-                apiview = CropProduceApiView
+                api_view = CropProduceApiView
 
             product = list_params[2]
             query_set = LivestockByproduct.objects.filter(name__icontains=product, sub_class="product")
             if query_set.count() > 0:
                 command_text = command_text.replace("產量", "產量（副產物產量）")
-                apiview = LivestockByproductApiView
+                api_view = LivestockByproductApiView
 
             query_set = CropProduceTotal.objects.filter(name__icontains=product)
             if query_set.count() > 0:
                 command_text = command_text.replace("產量", "產量（作物產量）")
-                apiview = CropProduceApiView
+                api_view = CropProduceApiView
 
-            if apiview is None:
-                reply = f"無法搜尋「{command_text}」\n" + f"查無品項，請修改品項關鍵字後重新查詢"
-                return reply
+        if not api_view:
+            return f"無法搜尋「{command_text}」\n" + f"查無品項，請修改品項關鍵字後重新查詢。"
 
     elif "指令" in command_text:
         reply = f"直接輸入指令可以查詢使用方式(括號內為備註不需輸入)，目前提供的指令如下\n\n"
@@ -148,19 +149,18 @@ def api_view(command_text):
         reply += "作物代碼"
         return reply
     else:
-        reply = f"很抱歉，本系統不支援您輸入的指令「{command_text}」，無法為您查詢，請輸入「指令」查詢目前可用的指令。"
-        return reply
+        return f"很抱歉，本系統不支援您輸入的指令「{command_text}」，無法為您查詢，請輸入「指令」查詢目前可用的指令。"
 
     try:
-        obj = apiview(list_params)
+        obj = api_view(list_params)
         if hasattr(obj, 'choose_api'):
             obj = obj.choose_api()
         reply = f"搜尋「{command_text}」結果如下\n" + obj.execute_api()
     except CustomError as ce:
         reply = f"無法搜尋「{command_text}」\n" + str(ce)
     except Exception as e:
-        traceback_log = TracebackLog.objects.create(app="api_view", message=traceback.format_exc())
-        reply = f"搜尋「{command_text}」發生未知錯誤，錯誤編號「{traceback_log.id}」，請通知管理員處理"
+        traceback_log = TracebackLog.objects.create(app="get_reply_from_text", message=traceback.format_exc())
+        reply = f"搜尋「{command_text}」發生未知錯誤，錯誤編號「{traceback_log.id}」，請通知管理員處理。"
         raise CustomError(reply)
 
     return reply
