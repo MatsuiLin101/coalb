192431d94e51d4a028c9cb47e82009010fe763c4
MatsuiLin101 <matsuilin101@gmail.com>
2025-02-11 15:46:56 +0800

    refactor: produce value builder

--- src/apps/coa/builders/producevalue.py
+++ src/apps/coa/builders/producevalue.py
@@ -2,46 +2,54 @@ from .configs import *
 
 
 class TotalValueBuilder(object):
-    '''
-    from apps.coa.builders.producevalue import *
-    a = TotalValueBuilder()
-    '''
+    """
+    建立 產值api介面 ProduceValueApiView 使用的選項
+
+from apps.coa.builders.producevalue import *
+builder = TotalValueBuilder()
+builder.build()
+    """
     def __init__(self):
         self.driver = None
-        self.url = "https://agrstat.moa.gov.tw/sdweb/public/inquiry/InquireAdvance.aspx"
-        self.text_title = "農業產值結構與指標"
-        self.text_group = "農業產值：縣市別×農業別"
-        self.id_group = "ctl00_cphMain_uctlInquireAdvance_lstFieldGroup"
-        self.id_city = "ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl00_lstDimension"
-        self.id_category = "ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl02_lstDimension"
+        self.url = 'https://agrstat.moa.gov.tw/sdweb/public/inquiry/InquireAdvance.aspx'
+        self.text_title = '農業產值結構與指標'
+        self.text_group = '農業產值：縣市別×農業別'
+        self.id_group = 'ctl00_cphMain_uctlInquireAdvance_lstFieldGroup'
+        self.id_city = 'ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl00_lstDimension'
+        self.id_category = 'ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl02_lstDimension'
 
     def build(self):
-        TotalValue.objects.all().delete()
-        self.driver = get_driver()
-        self.driver.get(self.url)
-        # 進入農業產值結構與指標頁面
-        self.driver.find_element(By.LINK_TEXT, self.text_title).click()
-        self.build_city(self.text_group)
-        self.build_category(self.text_group)
-        self.driver.close()
+        try:
+            TotalValue.objects.all().delete()
+            self.driver = get_driver()
+            self.driver.get(self.url)
+            # 進入農業產值結構與指標頁面
+            self.driver.find_element(By.LINK_TEXT, self.text_title).click()
+            self.build_city(self.text_group)
+            self.build_category(self.text_group)
+        except Exception as e:
+            print(traceback.format_exc())
+        finally:
+            if self.driver:
+                self.driver.quit()
 
     def build_city(self, text_group):
         # 選擇主分類
-        driver_select(self.driver, self.id_group, "text", text_group)
+        driver_select(self.driver, self.id_group, 'text', text_group)
         # 取得次分類城市
         time.sleep(1)
         group_city = self.driver.find_element(By.ID, self.id_city)
-        options = group_city.find_elements_by_tag_name("option")
+        options = group_city.find_elements(By.TAG_NAME, 'option')
         parent = None
         for option in options:
-            value = option.get_attribute("value")
+            value = option.get_attribute('value')
             name = option.text
             level = int(value.split('\t')[-1])
             search_name = name
 
             if parent is not None:
                 if level - parent.level != 1:
-                    parent = TotalValue.objects.filter(main_class=text_group, sub_class="city", level=level-1).last()
+                    parent = TotalValue.objects.filter(main_class=text_group, sub_class='city', level=level-1).last()
 
                 origin_parent = parent
                 while parent.level > 1:
@@ -53,7 +61,7 @@ class TotalValueBuilder(object):
             obj = TotalValue.objects.create(
                 parent = parent,
                 main_class = text_group,
-                sub_class = "city",
+                sub_class = 'city',
                 level = level,
                 name = name,
                 value = value,
@@ -67,10 +75,10 @@ class TotalValueBuilder(object):
         # 取得次分類產品
         time.sleep(1)
         group_category = self.driver.find_element(By.ID, self.id_category)
-        options = group_category.find_elements_by_tag_name("option")
+        options = group_category.find_elements(By.TAG_NAME, 'option')
         parent = None
         for option in options:
-            value = option.get_attribute("value")
+            value = option.get_attribute('value')
             name = option.text
             level = int(value.split('\t')[-1])
             search_name = name
@@ -80,7 +88,7 @@ class TotalValueBuilder(object):
 
             if parent is not None:
                 if level - parent.level != 1:
-                    parent = TotalValue.objects.filter(main_class=text_group, sub_class="category", level=level-1).last()
+                    parent = TotalValue.objects.filter(main_class=text_group, sub_class='category', level=level-1).last()
 
                 origin_parent = parent
                 while True:
@@ -95,7 +103,7 @@ class TotalValueBuilder(object):
             obj = TotalValue.objects.create(
                 parent = parent,
                 main_class = text_group,
-                sub_class = "category",
+                sub_class = 'category',
                 level = level,
                 name = name,
                 value = value,
