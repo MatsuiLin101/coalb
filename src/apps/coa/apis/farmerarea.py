696435f1440d68dfbaa7af12c39d0814408b8081
MatsuiLin101 <matsuilin101@gmail.com>
2025-02-11 16:23:35 +0800

    feat: review farmer area api

--- src/apps/coa/apis/farmerarea.py
+++ src/apps/coa/apis/farmerarea.py
@@ -2,18 +2,21 @@ from .configs import *
 
 
 class FarmerAreaApiView(AnnualReportBasicApiView):
-    '''
+    """
+    之後再修 需要先處理 LIBREOFFICE
+
     耕地面積api介面
-    farmerarea(耕地面積)
-    年報八、(一)、2
-    https://agrstat.moa.gov.tw/sdweb/public/book/Book.aspx
-    '''
+
+    - FarmerArea(耕地面積)
+        年報八、(一)、2
+        https://agrstat.moa.gov.tw/sdweb/public/book/Book.aspx
+    """
     def __init__(self, params):
         super(FarmerAreaApiView, self).__init__(params)
-        self.id_ods = "ctl00_cphMain_uctlBook_repChapter_ctl52_dtlFile_ctl00_lnkFile"
+        self.id_ods = 'ctl00_cphMain_uctlBook_repChapter_ctl52_dtlFile_ctl00_lnkFile'
 
         if not len(params) == 2:
-            raise CustomError(f"耕地面積的指令為「耕地面積 年份」，例如：\n「耕地面積 106」")
+            raise CustomError('耕地面積的指令為「耕地面積 年份」，例如：\n「耕地面積 106」')
         self.command = params[0]
         self.query_date = params[1]
 
