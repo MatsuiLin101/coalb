9f616a53102fe25fb2cf0a0063c1234fb243d0a4
MatsuiLin101 <matsuilin101@gmail.com>
2025-03-01 21:55:09 +0800

    refactor: livestock feedlot builder

--- src/apps/coa/builders/livestockfeedlot.py
+++ src/apps/coa/builders/livestockfeedlot.py
@@ -2,49 +2,57 @@ from .configs import *
 
 
 class LivestockFeedlotBuilder(object):
-    '''
-    from apps.coa.builders.livestockfeedlot import *
-    a = LivestockFeedlotBuilder()
-    '''
+    """
+    建立 畜禽飼養場數api介面 LivestockFeedlotApiView 使用的選項
+
+from apps.coa.builders.livestockfeedlot import *
+builder = LivestockFeedlotBuilder()
+builder.build()
+    """
     def __init__(self):
         self.driver = None
-        self.url = "https://agrstat.moa.gov.tw/sdweb/public/inquiry/InquireAdvance.aspx"
-        self.text_title = "畜禽產品飼養數量統計"
-        self.text_group1 = "家畜飼養場數"
-        self.text_group2 = "家禽飼養場數：縣市別×家禽別(104年度起)"
-        self.id_group = "ctl00_cphMain_uctlInquireAdvance_lstFieldGroup"
-        self.id_city = "ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl00_lstDimension"
-        self.id_product = "ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl02_lstDimension"
+        self.url = 'https://agrstat.moa.gov.tw/sdweb/public/inquiry/InquireAdvance.aspx'
+        self.text_title = '畜禽產品飼養數量統計'
+        self.text_group1 = '家畜飼養場數'
+        self.text_group2 = '家禽飼養場數：縣市別×家禽別'
+        self.id_group = 'ctl00_cphMain_uctlInquireAdvance_lstFieldGroup'
+        self.id_city = 'ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl00_lstDimension'
+        self.id_product = 'ctl00_cphMain_uctlInquireAdvance_dtlDimension_ctl02_lstDimension'
 
     def build(self):
-        LivestockFeedlot.objects.all().delete()
-        self.driver = get_driver()
-        self.driver.get(self.url)
-        # 進入畜禽產品飼養數量統計頁面
-        self.driver.find_element(By.LINK_TEXT, self.text_title).click()
-        self.build_city(self.text_group1)
-        self.build_product(self.text_group1)
-        self.build_city(self.text_group2)
-        self.build_product(self.text_group2)
-        self.driver.close()
+        try:
+            LivestockFeedlot.objects.all().delete()
+            self.driver = get_driver()
+            self.driver.get(self.url)
+            # 進入畜禽產品飼養數量統計頁面
+            self.driver.find_element(By.LINK_TEXT, self.text_title).click()
+            self.build_city(self.text_group1)
+            self.build_product(self.text_group1)
+            self.build_city(self.text_group2)
+            self.build_product(self.text_group2)
+        except Exception:
+            print(traceback.format_exc())
+        finally:
+            if self.driver:
+                self.driver.quit()
 
     def build_city(self, text_group):
         # 選擇主分類
-        driver_select(self.driver, self.id_group, "text", text_group)
+        driver_select(self.driver, self.id_group, 'text', text_group)
         # 取得次分類城市
         time.sleep(1)
         group_city = self.driver.find_element(By.ID, self.id_city)
-        options = group_city.find_elements_by_tag_name("option")
+        options = group_city.find_elements(By.TAG_NAME, 'option')
         parent = None
         for option in options:
-            value = option.get_attribute("value")
+            value = option.get_attribute('value')
             name = option.text
             level = int(value.split('\t')[-1])
             search_name = name
 
             if parent is not None:
                 if level - parent.level != 1:
-                    parent = LivestockFeedlot.objects.filter(main_class=text_group, sub_class="city", level=level-1).last()
+                    parent = LivestockFeedlot.objects.filter(main_class=text_group, sub_class='city', level=level-1).last()
 
                 origin_parent = parent
                 while parent.level > 1:
@@ -56,7 +64,7 @@ class LivestockFeedlotBuilder(object):
             obj = LivestockFeedlot.objects.create(
                 parent = parent,
                 main_class = text_group,
-                sub_class = "city",
+                sub_class = 'city',
                 level = level,
                 name = name,
                 value = value,
@@ -70,10 +78,10 @@ class LivestockFeedlotBuilder(object):
         # 取得次分類產品
         time.sleep(1)
         group_product = self.driver.find_element(By.ID, self.id_product)
-        options = group_product.find_elements_by_tag_name("option")
+        options = group_product.find_elements(By.TAG_NAME, 'option')
         parent = None
         for option in options:
-            value = option.get_attribute("value")
+            value = option.get_attribute('value')
             name = option.text
             level = int(value.split('\t')[-1])
             search_name = name
@@ -83,7 +91,7 @@ class LivestockFeedlotBuilder(object):
 
             if parent is not None:
                 if level - parent.level != 1:
-                    parent = LivestockFeedlot.objects.filter(main_class=text_group, sub_class="product", level=level-1).last()
+                    parent = LivestockFeedlot.objects.filter(main_class=text_group, sub_class='product', level=level-1).last()
 
                 origin_parent = parent
                 while True:
@@ -98,7 +106,7 @@ class LivestockFeedlotBuilder(object):
             obj = LivestockFeedlot.objects.create(
                 parent = parent,
                 main_class = text_group,
-                sub_class = "product",
+                sub_class = 'product',
                 level = level,
                 name = name,
                 value = value,
