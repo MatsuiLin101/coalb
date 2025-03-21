3cf9032f1cf47bcebc17622eab458675298bb2df
MatsuiLin101 <matsuilin101@gmail.com>
2025-03-21 13:14:53 +0800

    refactor: product code api switch to moa from coa

--- src/apps/coa/apis/productcode.py
+++ src/apps/coa/apis/productcode.py
@@ -2,29 +2,33 @@ from .configs import *
 
 
 class ProductCodeApiView(BasicApiView):
-    '''
+    """
     作物代碼api介面
-    (NEW)productcode(作物代碼)
-    EXCEL匯入
-    '''
+
+    - (NEW)ProductCode(作物代碼)
+        必需先使用 LINE 上傳功能匯入「主力勞動力代碼對照」
+    """
     def __init__(self, params):
-        self.message = ""
+        self.message = ''
         if not len(params) == 2:
-            raise CustomError(f"作物代碼的指令為「代碼 品項」，例如：\n「代碼 甘藍」")
+            raise CustomError('作物代碼的指令為「代碼 品項」，例如：\n「代碼 甘藍」')
         self.command, self.product = params
-        self.command_text = " ".join(text for text in params)
+        self.command_text = ' '.join(text for text in params)
+
+    def get_data(self):
+        pass
 
     def execute_api(self):
         try:
             query_set = ProductCode.objects.filter(name__icontains=self.product)
             if query_set.count() == 0:
-                self.message = f"「{self.command_text}」查無結果"
+                self.message = f'「{self.command_text}」查無結果'
             else:
                 message_list = list()
                 for obj in query_set:
-                    message_list.append(f"{obj.category}：{obj.code} {obj.name}")
-                self.message = f"{self.product} 代碼：\n" + "\n".join(message for message in message_list)
+                    message_list.append(f'{obj.category}：{obj.code} {obj.name}')
+                self.message = f'{self.product} 代碼：\n' + '\n'.join(message for message in message_list)
         except Exception as e:
-            traceback_log = TracebackLog.objects.create(app=f"{self.classname}", message=traceback.format_exc())
-            self.message = f"「{self.command_text}」發生未知錯誤，錯誤編號「{traceback_log.id}」，請通知管理員處理"
+            traceback_log = TracebackLog.objects.create(app=f'{self.classname}', message=traceback.format_exc())
+            self.message = f'「{self.command_text}」發生未知錯誤，錯誤編號「{traceback_log.id}」，請通知管理員處理'
         return self.message
